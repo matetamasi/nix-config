bb988f650de3aacc9158b5af8bcd414d9e1b615e
Tamási Máté <matetamasi@protonmail.com>
2025-11-21 11:00:57 +0100

    Persist .eclipse

--- persist.nix
+++ persist.nix
@@ -78,6 +78,9 @@
 
         # Direnv
         ".local/share/direnv/allow"
+
+        # Eclipse
+        ".eclipse"
       ];
       files = [
         ".local/state/cosmic-comp/outputs.ron" # Cosmic display configuration
