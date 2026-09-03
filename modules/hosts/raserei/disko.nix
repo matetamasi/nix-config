6970ae9dd34a169561f8fd52bdbbc841d21fb33a
Tamási Máté <matetamasi@protonmail.com>
2026-09-03 19:46:31 +0200

    Configure disko for raserei

--- modules/hosts/raserei/disko.nix
+++ modules/hosts/raserei/disko.nix
@@ -1,33 +1,113 @@
 _: {
   flake.modules.raserei."disko" = _: {
     disko.devices = {
-      disk.main = {
-        device = "/dev/nvme0n1"; # Placeholder
-        type = "disk";
-        content = {
-          type = "gpt";
-          partitions = {
-            boot = {
-              size = "1M";
-              type = "EF02"; # for grub MBR
-            };
-            ESP = {
-              size = "1G";
-              type = "EF00";
-              content = {
-                type = "filesystem";
-                format = "vfat";
-                mountpoint = "/boot";
-                mountOptions = ["defaults" "umask=0077"];
+      disk = {
+        main = {
+          device = "/dev/disk/by-id/nvme-eui.01000000000000008ce38e0400091afe";
+          type = "disk";
+          content = {
+            type = "gpt";
+            partitions = {
+              ESP = {
+                size = "1G";
+                type = "EF00";
+                content = {
+                  type = "filesystem";
+                  format = "vfat";
+                  mountpoint = "/boot";
+                  mountOptions = ["defaults" "umask=0077"];
+                };
+              };
+              root = {
+                size = "100%";
+                content = {
+                  type = "filesystem";
+                  format = "ext4";
+                  mountpoint = "/";
+                };
               };
             };
-            root = {
+          };
+        };
+
+        data1 = {
+          device = "/dev/disk/by-id/wwn-0x5000c500a6633d4b";
+          type = "disk";
+          content = {
+            type = "gpt";
+            partitions.zfs = {
               size = "100%";
               content = {
-                type = "filesystem";
-                format = "ext4";
-                mountpoint = "/";
+                type = "zfs";
+                pool = "tank";
               };
+            };
+          };
+        };
+
+        data2 = {
+          device = "/dev/disk/by-id/wwn-0x5000c500955d123f";
+          type = "disk";
+          content = {
+            type = "gpt";
+            partitions.zfs = {
+              size = "100%";
+              content = {
+                type = "zfs";
+                pool = "tank";
+              };
+            };
+          };
+        };
+
+        data3 = {
+          device = "/dev/disk/by-id/wwn-0x5000c500955cc68f";
+          type = "disk";
+          content = {
+            type = "gpt";
+            partitions.zfs = {
+              size = "100%";
+              content = {
+                type = "zfs";
+                pool = "tank";
+              };
+            };
+          };
+        };
+
+        data4 = {
+          device = "/dev/disk/by-id/wwn-0x5000c500a6eb7ac3";
+          type = "disk";
+          content = {
+            type = "gpt";
+            partitions.zfs = {
+              size = "100%";
+              content = {
+                type = "zfs";
+                pool = "tank";
+              };
+            };
+          };
+        };
+      };
+
+      zpool = {
+        tank = {
+          type = "zpool";
+          mode = "raidz1";
+          rootFsOptions = {
+            compression = "zstd";
+            "com.sun:auto-snapshot" = "false";
+          };
+          mountpoint = "/tank";
+          datasets = {
+            root = {
+              type = "zfs_fs";
+              mountpoint = "/tank/data";
+            };
+            backups = {
+              type = "zfs_fs";
+              mountpoint = "/tank/backups";
             };
           };
         };
